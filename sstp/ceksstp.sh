fae3d18a02f8002652862814670b88f350a36fc6
Lakmal <73278284+sandaru-star@users.noreply.github.com>
2022-11-13 20:45:49 +0530

    Add files via upload

---
+++ sstp/ceksstp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# My Telegram : https://t.me/SLvpn1
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/sandaru-star/SLv2ray/main/ipvps.txt | grep $MYIP )
+if [ $MYIP = $MYIP ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}"
+exit 0
+fi
+clear
+accel-cmd show sessions
+echo ""
